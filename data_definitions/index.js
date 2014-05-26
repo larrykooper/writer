34a33bb4ca297284d298a4f3755a7a3b32c8ea1d
larrykooper <larry.kooper@gmail.com>
2014-05-26 01:29:37 -0400

    Fixed bugs in logic for test environment

--- data_definitions/index.js
+++ data_definitions/index.js
@@ -1,17 +1,10 @@
 if (!global.hasOwnProperty('db')) {
     var Sequelize = require('sequelize');
     var sequelize = null;
-    var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+    var match;
 
-    sequelize = new Sequelize(match[5], match[1], match[2], {
-            dialect:  'postgres',
-            protocol: 'postgres',
-            port:     match[4],
-            host:     match[3],
-            logging:  console.log
-        });
-
-    if (global.env == 'test') {
+    if (process.env.NODE_ENV == 'test') {
+        console.log('IT IS TEST');
 
         match = process.env.TEST_DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
 
@@ -25,16 +18,28 @@ if (!global.hasOwnProperty('db')) {
 
         global.db = {
             Sequelize: Sequelize,
-            sequelize: sequelize_test,
-            User:      sequelize.import(__dirname + '/user')
+            sequelize: sequelize_test
         }
     } else {
+        match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+
+        sequelize = new Sequelize(match[5], match[1], match[2], {
+            dialect:  'postgres',
+            protocol: 'postgres',
+            port:     match[4],
+            host:     match[3],
+            logging:  console.log
+        });
+
+        console.log("IT IS NOT TEST");
         global.db = {
             Sequelize: Sequelize,
-            sequelize: sequelize,
-            User:      sequelize.import(__dirname + '/user')
+            sequelize: sequelize
         }
     }
+
+    global.db.User = global.db.sequelize.import(__dirname + '/user');
+    global.db.Post = global.db.sequelize.import(__dirname + '/post');
 
 }
 module.exports = global.db
