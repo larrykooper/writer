cca43e8c6290f9209de5a6b6f495797ce8aaef79
larrykooper <larry.kooper@gmail.com>
2014-05-26 01:30:41 -0400

    Added definitions for the second and third steps in the feature

--- features/step_definitions/add-post.steps.js
+++ features/step_definitions/add-post.steps.js
@@ -1,18 +1,32 @@
 'use strict';
-global.env = 'test';
+
 var user = require(process.cwd() + '/models/user');
+var post = require(process.cwd() + '/models/post');
+
 module.exports = function() {
+
+    this.World = require("../support/world.js").World; // overwrite default World constructor
     
     var myUser;
+    var myPosting;
     
     this.Given(/^there is a User$/, function (done) {
         myUser = user.create({username: 'john'});
         done();
     });
 
-    this.Given(/^the User has posted the posting "([^"]*)"$/, function (arg1, callback) {
+    this.Given(/^the User has posted the posting "([^"]*)"$/, function (arg1, done) {
+        myPosting = post.create({
+            title: "Test Post Title",
+            body: arg1
+        })
+        done();
+    });
+
+    this.When(/^I visit the homepage$/, function (callback) {
         // express the regexp above with the code you wish you had
-        callback.pending();
+        this.visit("http://localhost:3000/", callback);
+
     });
 
 }
