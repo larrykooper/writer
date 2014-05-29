cf4f2fb714f68de2454b7c1e59b0a2d785231b14
larrykooper <larry.kooper@gmail.com>
2014-05-29 18:53:11 -0400

    User authentication

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,27 @@
+var db = require('../models');
+
+function storeauth(err, req, res, user) {
+    // Returns user here if user is authenticated
+    // Delegate errors
+    if (err) {
+        console.error(err);
+    };
+    if (user) {
+        // Store user ID in session
+        req.session.uid = user.id;
+        res.redirect('admin/posts');
+    } else {
+        res.error("Your username or password is incorrect");
+        res.redirect('back');
+    }
+}
+
 exports.submit = function(req, res, next) {
-    res.redirect('admin/posts');
+    var user = db.User.build({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    user.authenticate(user.username, user.password, storeauth);
+
 }
