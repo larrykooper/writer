84628e1884b552d61ddce1aeaf0a8c004368d5e0
larrykooper <larry.kooper@gmail.com>
2014-05-29 20:47:08 -0400

    Made the callback for authenticate an inner function because I needed this.req

--- routes/login.js
+++ routes/login.js
@@ -1,27 +1,28 @@
 var db = require('../models');
-
-function storeauth(err, req, res, user) {
-    // Returns user here if user is authenticated
-    // Delegate errors
-    if (err) {
-        console.error(err);
-    };
-    if (user) {
-        // Store user ID in session
-        req.session.uid = user.id;
-        res.redirect('admin/posts');
-    } else {
-        res.error("Your username or password is incorrect");
-        res.redirect('back');
-    }
-}
+var express = require('express');
+var res = express.response;
 
 exports.submit = function(req, res, next) {
+    var session = req.session;
     var user = db.User.build({
         username: req.body.username,
         password: req.body.password
     });
 
-    user.authenticate(user.username, user.password, storeauth);
+    user.authenticate(user.username, user.password, session, function(err, session, user){
+        // Returns user here if user is authenticated
+        // Delegate errors
+        if (err) {
+            console.error(err);
+        };
+        if (user) {
+            // Store user ID in session
+            session.uid = user.id;
+            res.redirect('admin/posts');
+        } else {
+            res.error("Your username or password is incorrect", session);
+            res.redirect('/admin');
+        }
+    });
 
 }
