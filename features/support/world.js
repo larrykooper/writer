cbefe7d118e2e38a04d8e203dbc7258abe8ba5ef
Larry Kooper <larry.kooper@gmail.com>
2014-06-05 16:58:33 -0400

    Fixed indentation

--- features/support/world.js
+++ features/support/world.js
@@ -6,12 +6,12 @@ db.Post.deleteAll();
 db.User.deleteAll();
 
 var World = function World(callback) {
-  this.browser = new zombie(); // this.browser will be available in step definitions
+    this.browser = new zombie(); // this.browser will be available in step definitions
 
-  this.visit = function(url, callback) {
-    this.browser.visit(url, callback);
-  };
+    this.visit = function(url, callback) {
+        this.browser.visit(url, callback);
+    };
 
-  callback(); // tell Cucumber we're finished and to use 'this' as the world instance
+    callback(); // tell Cucumber we're finished and to use 'this' as the world instance
 };
 exports.World = World;
