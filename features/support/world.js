391055e513840991a604ec869f220cf36902d0d5
Larry Kooper <larry.kooper@gmail.com>
2014-06-05 17:52:12 -0400

    Added a login function to World

--- features/support/world.js
+++ features/support/world.js
@@ -12,6 +12,21 @@ var World = function World(callback) {
         this.browser.visit(url, callback);
     };
 
+    this.login = function(callback) {
+        //console.log("I AM LOGGING IN");
+        var myUser;
+        myUser = db.User.create({
+            username: "mary",
+            password: "catdog"
+        }, callback);
+        var context = this;
+        this.visit("http://localhost:3000/admin", function(){
+            context.browser.fill(".email", myUser.username).fill(".password", myUser.password);
+            context.browser.pressButton("Login", function() {
+            });
+        });
+    };
+
     callback(); // tell Cucumber we're finished and to use 'this' as the world instance
 };
 exports.World = World;
