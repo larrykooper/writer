eb172a7cd807e3b59410dd86ce88ce8b1f717a4e
larrykooper <larry.kooper@gmail.com>
2014-06-06 22:12:48 -0400

    Cleaned up world based on antonydenyer/zombiejsplayground

--- features/support/world.js
+++ features/support/world.js
@@ -1,9 +1,13 @@
 var zombie = require('zombie');
 var db = require('../../models');
+var util = require('./util');
+var fs = require('fs');
+var path = require('path');
 
 // clear out test database
 db.Post.deleteAll();
-db.User.deleteAll();
+// Not deleting users so I can keep a test user
+//db.User.deleteAll();
 
 var World = function World(callback) {
     this.browser = new zombie(); // this.browser will be available in step definitions
@@ -12,21 +16,26 @@ var World = function World(callback) {
         this.browser.visit(url, callback);
     };
 
-    this.login = function(callback) {
-        //console.log("I AM LOGGING IN");
-        var myUser;
-        myUser = db.User.create({
-            username: "mary",
-            password: "catdog"
-        }, callback);
-        var context = this;
-        this.visit("http://localhost:3000/admin", function(){
-            context.browser.fill(".email", myUser.username).fill(".password", myUser.password);
-            context.browser.pressButton("Login", function() {
-            });
-        });
+    this.helpers = {
+        browser: this.browser
     };
+
+    load(path.join(__dirname, './helpers'), this.helpers);
 
     callback(); // tell Cucumber we're finished and to use 'this' as the world instance
 };
+
+function load(helperPath, dest) {
+    var files = fs.readdirSync(helperPath),
+    modulePath;
+
+    for (var i = 0; i < files.length; i++) {
+        if (files[i].match(/\.swp$/)) {
+            continue;
+        }
+        modulePath = path.join(helperPath, files[i]);
+        util.mixin(require(modulePath), dest);;
+    }
+}
+
 exports.World = World;
