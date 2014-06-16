641e8d736b7abd85fd97f21f469eed25a46bba05
larrykooper <larry.kooper@gmail.com>
2014-06-15 23:49:41 -0400

    Client-side support for deleting a post (in progress)

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,18 @@
+var myNamespace = (function () {
+
+    var doDeleteCall;
+
+    doDeleteCall = function(foo) {
+        // THE NUMBER 5 IS JUST A PLACEHOLDER. TODO: UPDATE THIS
+        $.ajax({
+            type: "DELETE",
+            url: "/admin/posts/5.json"
+        })
+        .done(function(msg) {
+            alert("Data has been deleted!" + msg);
+        });
+    };
+
 $(document).ready(function() {
     
     $("#newPost").on("click", function() {
@@ -5,9 +20,11 @@ $(document).ready(function() {
     })
 
     $("a[kind='delete']").on("click", function() {
+        var postNumber = 7; // THE 7 IS JUST A PLACEHOLDER. TODO: UPDATE THIS
         var response=confirm("Are you sure you want to delete this post?");
         if (response) {
             console.log("You pressed OK");
+            doDeleteCall(postNumber);
         } else {
             console.log("You pressed cancel");
         }
@@ -22,3 +39,5 @@ $(document).ready(function() {
     );
 
 });
+
+}());
