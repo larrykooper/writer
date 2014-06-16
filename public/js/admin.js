d90b6a2997a1610a5d889fd4f3e58e388c7439b6
larrykooper <larry.kooper@gmail.com>
2014-06-15 23:04:25 -0400

    Posts page - underline Delete

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     
-    $("#newPost").on( "click", function() {
+    $("#newPost").on("click", function() {
         window.location.replace('/admin/editor');
     })
-    
+
+    $("a[kind='delete']").on("click", function() {
+        alert("Are you sure you want to delete the post?");
+    })
+
+    $("a[kind='delete']").hover(
+        function() {
+            $(this).css("text-decoration", "underline");
+        }, function() {
+            $(this).css("text-decoration", "none");
+        }
+    );
+
 });
