68d304a818963ed0d2bcfe572053c0ba047b29d8
larrykooper <larry.kooper@gmail.com>
2014-06-15 20:45:14 -0400

    Sort posts by reverse chron

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var db = require('../models');
 
 exports.index = function(req, res){
-  db.Post.findAll().success(function(posts) {
+  db.Post.findAll({order: '"createdAt" DESC'}).success(function(posts) {
     res.render('index', {
       title: 'Writer',
       posts: posts
